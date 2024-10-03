274209a4504ebef1c8adee6bac97bc42d66ced81
devyn-miller <144622580+devyn-miller@users.noreply.github.com>
2024-10-03 10:24:44 -0700

    update params

--- src/training/config.py
+++ src/training/config.py
@@ -6,6 +6,7 @@ PERIODS = 100
 STARTING_PRICES = [300, 300, 300]
 LARGEST_DISCOUNT = 30  # most you can decrease your prices in one period
 LARGEST_INCREASE = 30   # most you can increase your prices in one period
+# INERTIA = min(prices)/10
 INERTIA = 10**10
 
 # Training parameters
